a514c8917b5b0b178de351e0236c08ba63fc7515
Andreszr <oandreszr@gmail.com>
2016-03-27 22:47:36 -0500

    Structure

--- app/src/main/java/co/edu/udea/loaiza/sivan/com/gmail/oandreszr/evaludea/MainActivity.java
+++ app/src/main/java/co/edu/udea/loaiza/sivan/com/gmail/oandreszr/evaludea/MainActivity.java
@@ -1,5 +1,7 @@
 package co.edu.udea.loaiza.sivan.com.gmail.oandreszr.evaludea;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,22 +13,15 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/co/edu/udea/loaiza/sivan/com/gmail/oandreszr/evaludea/MainActivityFragment.java
+++ app/src/main/java/co/edu/udea/loaiza/sivan/com/gmail/oandreszr/evaludea/MainActivityFragment.java
@@ -1,22 +1,43 @@
 package co.edu.udea.loaiza.sivan.com.gmail.oandreszr.evaludea;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import static android.R.layout.simple_spinner_item;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private String[] arraySpinner;
+
     public MainActivityFragment() {
+        // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        /*this.arraySpinner = new String[] {
+                "Básico", "Intermedio", "Real"
+        };
+        Spinner spinner = (Spinner) view.findViewById(R.id.mainSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), simple_spinner_item, arraySpinner);
+        spinner.setAdapter(adapter);*/
+
+        return view;
     }
 }
